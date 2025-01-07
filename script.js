993511c0638155feecbe858b1a9689f5b23bc1ae
khonde janhavi sharad <150101301+janhavikhonde2010@users.noreply.github.com>
2025-01-07 18:15:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCu6hde7chUUiO56AVSjjxp6sayC1aM8ZM",
+    authDomain: "phone-1398c.firebaseapp.com",
+    projectId: "phone-1398c",
+    storageBucket: "phone-1398c.firebasestorage.app",
+    messagingSenderId: "1056210006005",
+    appId: "1:1056210006005:web:48b7e9b005fabfef11babc",
+    measurementId: "G-NFXNDRDLGN"
+  };
+// initializing firebase SDK
+firebase.initializeApp(firebaseConfig);
+
+// render recaptcha verifier
+render();
+function render() {
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+    recaptchaVerifier.render();
+}
+
+// function for send OTP
+function sendOTP() {
+    var number = document.getElementById('number').value;
+    firebase.auth().signInWithPhoneNumber(number, window.recaptchaVerifier).then(function (confirmationResult) {
+        window.confirmationResult = confirmationResult;
+        coderesult = confirmationResult;
+        document.querySelector('.number-input').style.display = 'none';
+        document.querySelector('.verification').style.display = '';
+    }).catch(function (error) {
+        // error in sending OTP
+        alert(error.message);
+    });
+}
+
+// function for OTP verify
+function verifyCode() {
+    var code = document.getElementById('verificationCode').value;
+    coderesult.confirm(code).then(function () {
+        document.querySelector('.verification').style.display = 'none';
+        document.querySelector('.result').style.display = '';
+        document.querySelector('.correct').style.display = '';
+        console.log('OTP Verified');
+    }).catch(function () {
+        document.querySelector('.verification').style.display = 'none';
+        document.querySelector('.result').style.display = '';
+        document.querySelector('.incorrect').style.display = '';
+        console.log('OTP Not correct');
+    })
+}
